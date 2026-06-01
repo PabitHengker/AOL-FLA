24362b0a0bc635b5783d10172a07fa9a534fecf2
PabitHengker <panitsentanabakti@gmail.com>
2026-06-01 11:27:15 +0700

    adding frameworks to repo

---
+++ backend/src/test/java/com/FLA/LibraryProject/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FLA.LibraryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
